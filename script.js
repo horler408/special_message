5ca3d1e36ae71c3df288c85890e34f3d8955df99
horler408 <horler.web@gmail.com>
2021-01-06 22:56:37 +0100

    changing prompt to confirm

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.getElementById("container");
 // const error = document.querySelector('.error');
-const name = prompt("Please enter your name").toLowerCase();
+//const name = prompt("Please enter your name").toLowerCase();
 
 
-if(name.includes("sekinat")) {
+if(window.confirm("Are you Sekinat Olaitan?")) {
 	container.style.display = "block"
 }else {
 	alert("This message is not meant for you, thank you!")
